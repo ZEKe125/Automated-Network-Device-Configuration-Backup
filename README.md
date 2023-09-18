77cc147804a2f39272d8435a1daf133dbcce7642
Ezequiel Cuevas <82134360+ZEKe125@users.noreply.github.com>
2023-09-18 13:22:34 -0400

    added .pem key auth

---
+++ README.md
@@ -0,0 +1,78 @@
+# Automated Network Device Configuration Backup
+
+Automated Network Device Configuration Backup is a Python script that connects to network devices (e.g., routers, switches, firewalls) using SSH key authentication, retrieves their configurations, and stores them in a Git repository. This script provides a reliable way to schedule regular backups of network device configurations and maintain version control for changes.
+
+## Table of Contents
+
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Customization](#customization)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Prerequisites
+
+Before using this script, make sure you have the following prerequisites in place:
+
+- Python 3.x installed on your system.
+- Required Python libraries installed (you can install them using `pip install paramiko gitpython smtplib`).
+
+## Installation
+
+1. Clone this repository to your local machine:
+
+   ```bash
+   git clone https://github.com/your_username/your_repo.git
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd automated-network-backup
+
+3. Install the required Python libraries:
+
+   ```bash
+   pip install paramiko gitpython smtplib
+
+## Usage
+
+To use the script, follow these steps:
+
+1. Configure the script:
+
+    - Update the network device details, Git repository details, and email notification settings in the script file (backup_script.py).
+
+2. Run the script:
+
+    - This will connect to the network devices, retrieve their configurations, and store them in the Git repository. If any errors occur, email notifications will be sent.
+
+   ```bash
+    python backup_script.py
+
+3. Schedule regular backups:
+
+    - You can schedule the script to run at specified intervals using a task scheduler or cron job, depending on your operating system.
+
+## Customization
+
+1. You can customize the script according to your specific requirements:
+
+- Modify the SSH command (`'show running-config'`) in the `backup_device_config()` function to match the command needed for your network devices.
+  - **('ip -s link show')**
+  - **('ifconfig')**
+  - **('ip addr')**
+- Add additional error handling or logging as needed.
+- Customize the email notification content and settings in the `send_email_notification()` function.
+
+## Contributing
+
+Contributions to this project are welcome. If you encounter issues, have ideas for improvements, or would like to contribute code, please open an issue or create a pull request on the GitHub repository.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+**Disclaimer:** Use this script responsibly and ensure it complies with your network device access policies and security guidelines. Always protect sensitive information and credentials.
---
+++ script_with_key_auth.py
@@ -0,0 +1,87 @@
+import paramiko
+import git
+import os
+import time
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# Define network device details
+device_ip = '192.168.1.1'
+device_port = 22
+device_username = 'your_username'
+private_key_path = '/path/to/your/ssh/key.pem'  # Path to your .pem private key file
+
+# Define Git repository details
+repo_path = '/path/to/your/git/repo'
+repo_url = 'https://github.com/your_username/your_repo.git'
+
+# Define email notification settings
+email_from = 'your_email@gmail.com'
+email_to = 'recipient@example.com'
+email_subject = 'Network Device Configuration Backup'
+
+# Function to connect to the network device and retrieve configuration
+def backup_device_config():
+    try:
+        # Create an SSH client and load the private key
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        private_key = paramiko.RSAKey.from_private_key_file(private_key_path)
+        
+        # Connect to the network device using SSH key authentication
+        ssh.connect(device_ip, port=device_port, username=device_username, pkey=private_key)
+        
+        # Send a command to retrieve the configuration (replace with your specific command)
+        stdin, stdout, stderr = ssh.exec_command('show running-config')
+        
+        # Read the configuration data
+        config_data = stdout.read().decode()
+        
+        # Close the SSH connection
+        ssh.close()
+        
+        return config_data
+    except Exception as e:
+        send_email_notification(f'Error: {str(e)}')
+        return None
+
+# Function to commit configuration to Git repository
+def commit_to_git(config_data):
+    try:
+        # Initialize the Git repository
+        repo = git.Repo(repo_path)
+        
+        # Create a Git commit with the configuration data
+        repo.index.add(['config.txt'])
+        repo.index.commit('Backup configuration')
+        
+        # Push the changes to the remote repository
+        origin = repo.remote('origin')
+        origin.push()
+    except Exception as e:
+        send_email_notification(f'Git Error: {str(e)}')
+
+# Function to send email notifications
+def send_email_notification(message):
+    msg = MIMEMultipart()
+    msg['From'] = email_from
+    msg['To'] = email_to
+    msg['Subject'] = email_subject
+    msg.attach(MIMEText(message, 'plain'))
+    
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email_from, 'your_email_password')
+    server.sendmail(email_from, email_to, msg.as_string())
+    server.quit()
+
+if __name__ == '__main__':
+    config_data = backup_device_config()
+    if config_data:
+        # Save configuration to a file (adjust the filename as needed)
+        with open(os.path.join(repo_path, 'config.txt'), 'w') as config_file:
+            config_file.write(config_data)
+        
+        # Commit and push to Git
+        commit_to_git(config_data)
